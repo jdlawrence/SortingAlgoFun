654156a6d09563ad4e8abf82366913766bd7202f
jdlawrence <jamil.lawrence@gmail.com>
2017-03-13 11:03:15 -0700

    Basic rules

---
+++ eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    'rules': {
+      'no-var': 0,
+      'vars-on-top': 0,
+      'no-unused-vars': 0,
+      'func-names': 0,
+      'no-else-return': 0,
+      'no-console': 0,
+      'no-param-reassign': 0
+    }
+};
