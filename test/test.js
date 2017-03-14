3f41c90a31c13757e34fb0ca0e7e924f54161b6f
jdlawrence <jamil.lawrence@gmail.com>
2017-03-14 15:02:41 -0700

    Adds test for merge helper function

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
 var expect = require('chai').expect;
 var bubbleSort = require('../src/bubbleSort');
+var merge = require('../src/helpers').merge;
 
 describe('BubbleSort', function () {
   it('Sorts an array in ascending order', () => {
@@ -9,3 +10,22 @@ describe('BubbleSort', function () {
     expect(sorted).to.eql([-6, 1, 3, 5, 7]);
   });
 });
+
+describe('The merge helper function', function () {
+  it('Merges in two arrays of a single element each', () => {
+    var merged = merge([7], [3])
+    expect(merged).to.eql([3, 7]);
+  })
+  it('Merges arrays of unequal length where the first is longer', () => {
+    var merged = merge([0, 1, 1, 2, 3, 5, 8, 13], [-3.1415927]);
+    expect(merged).to.eql([-3.1415927, 0, 1, 1, 2, 3, 5, 8, 13]);
+  });
+  it('Merges arrays of unequal length where the first is shorter', () => {
+    var merged = merge([-3.1415927], [0, 1, 1, 2, 3, 5, 8, 13]);
+    expect(merged).to.eql([-3.1415927, 0, 1, 1, 2, 3, 5, 8, 13]);
+  });
+  it('Merges arrays of unequal length where the first is empty', () => {
+    var merged = merge([], [0, 1, 1, 2, 3, 5, 8, 13]);
+    expect(merged).to.eql([0, 1, 1, 2, 3, 5, 8, 13]);
+  });
+})
