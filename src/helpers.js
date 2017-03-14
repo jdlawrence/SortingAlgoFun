8996a3217e7ff7b91cafc46e9bd7a956a0c4f54d
jdlawrence <jamil.lawrence@gmail.com>
2017-03-14 14:45:05 -0700

    Finish merge helper

--- src/helpers.js
+++ src/helpers.js
@@ -11,9 +11,27 @@ function swap(arr, first, second) {
 
 function merge(left, right) {
   var result = [];
+  var leftIndex = 0;
+  var rightIndex = 0;
 
+  while (leftIndex < left.length || rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex++]);
+    } else if (right[rightIndex] < left[leftIndex]) {
+      result.push(right[rightIndex++]);
+    } else if (leftIndex === left.length) {
+      result.push(right[rightIndex++]);
+    } else {
+      result.push(left[leftIndex++]);
+    }
+  }
   return result;
 }
+
+var left = [7];
+var right = [3];
+
+console.log(merge(left, right));
 
 module.exports = {
   merge: merge,
