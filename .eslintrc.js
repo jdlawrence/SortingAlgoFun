7f67a015a3e0431e8141d3920d89a8d8e7501c78
jdlawrence <jamil.lawrence@gmail.com>
2017-03-13 22:18:49 -0700

    Added . to file name

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+        "no-var": 0,
+        "vars-on-top": 0,
+        "no-unused-vars": 0,
+        "func-names": 0,
+        "no-else-return": 0,
+        "no-console": 0,
+        "no-param-reassign": 0,
+        "no-plusplus": 0,
+        "no-use-before-define": 0,
+        "react/jsx-sort-prop-types": 0
+    }
+};
