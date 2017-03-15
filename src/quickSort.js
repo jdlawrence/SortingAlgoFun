03f29f0a60ee99de6ebbe35fda8f6e53743aa9e8
jdlawrence <jamil.lawrence@gmail.com>
2017-03-14 22:28:36 -0700

    Working on quickSort

--- src/quickSort.js
+++ src/quickSort.js
@@ -1,9 +1,17 @@
 var partition = require('./helpers').partition;
 
-function quickSort(arr, lo, hi) {
-  var result = arr.slice(arr);
-  return result;
+// Note: quickSort sorts in place and manipulates the input array
+function quickSort(arr, left, right) {
+  left = left === undefined ? 0 : left;
+  right = right === undefined ? arr.length - 1 : right;
+  debugger;  
+  var pivotIndex = partition(arr, left, right);
+  quickSort(arr, left, pivotIndex - 1);
+  quickSort(arr, pivotIndex + 1, left);
+  return arr;
 }
+
+quickSort([4, 3, 2, 1]);
 
 module.exports = quickSort;
 
