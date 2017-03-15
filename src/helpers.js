7f4a2ff4875207a2a91a1c3cb9c3db4351b8c925
jdlawrence <jamil.lawrence@gmail.com>
2017-03-14 18:27:43 -0700

    Working on partition function

--- src/helpers.js
+++ src/helpers.js
@@ -28,7 +28,39 @@ function merge(left, right) {
   return result;
 }
 
+function partition(arr, left, right) {
+  var result = arr.slice();
+  var pivotIndex = 0;
+  var pivotVal = result[left];
+  left = left === undefined ? 1 : left;
+  right = right === undefined ? arr.length - 1 : right;
+
+  while (left < right) {
+    // Move right until you find a value greater than the pivot
+    while (result[left] <= pivotVal) {
+      left++;
+    }
+
+    // Move left until you find a value less than or equal to the pivot
+    while (result[right] > pivotVal) {
+      right--;
+    }
+
+    if (left < right) {
+      swap(result, left++, right--);
+    } else {
+      break;
+    }
+  }
+  swap(result, right, pivotIndex);
+  return result;
+}
+
+var dummy = [4, 7, 6, 1, 2];
+
+console.log(partition(dummy));
 module.exports = {
   merge: merge,
+  partition: partition,
   swap: swap
 };
