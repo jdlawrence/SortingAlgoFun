498c9168dad6aa8b2cd5e8e3a4ee26ba0936ccc8
jdlawrence <jamil.lawrence@gmail.com>
2017-03-15 09:37:56 -0700

    Added cases for empty array

--- src/quickSort.js
+++ src/quickSort.js
@@ -2,16 +2,21 @@ var partition = require('./helpers').partition;
 
 // Note: quickSort sorts in place and manipulates the input array
 function quickSort(arr, left, right) {
+  if (arr.length === 0) {
+    return [];
+  }
+  if (left >= right) {
+    return;
+  }
   left = left === undefined ? 0 : left;
   right = right === undefined ? arr.length - 1 : right;
-  debugger;  
   var pivotIndex = partition(arr, left, right);
   quickSort(arr, left, pivotIndex - 1);
-  quickSort(arr, pivotIndex + 1, left);
+  quickSort(arr, pivotIndex + 1, right);
   return arr;
 }
 
-quickSort([4, 3, 2, 1]);
+console.log(quickSort([5, 4, 7, -2, 6]));
 
 module.exports = quickSort;
 
